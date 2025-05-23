d9fe76f234954df8bb51cf4649fe8ef3ae3df343
gires667 <gires8585@gmail.com>
2025-05-23 16:27:00 +0200

    v1 : integration de mistral

---
+++ Backend.py
@@ -0,0 +1,55 @@
+import os
+from mistralai import Mistral
+
+import base64
+
+from dotenv import load_dotenv
+load_dotenv()
+
+def encode_image(image_path):
+    """Encode the image to base64."""
+    try:
+        with open(image_path, "rb") as image_file:
+            return base64.b64encode(image_file.read()).decode('utf-8')
+    except FileNotFoundError:
+        print(f"Error: The file {image_path} was not found.")
+        return None
+    except Exception as e:  # Added general exception handling
+        print(f"Error: {e}")
+        return None
+
+def describe_image(image_path):     
+
+    base64_image = encode_image(image_path)
+
+    api_key = os.environ["MISTRAL_API_KEY"]
+    model = "pixtral-12b-2409"
+
+    client = Mistral(api_key=api_key)
+
+    chat_response = client.chat.complete(
+        model = model,
+        messages = [
+            {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "decris moi l'image"
+                },
+                {
+                    "type": "image_url",
+                    "image_url": f"data:image/jpeg;base64,{base64_image}" 
+                }
+            ]
+        }
+        ]
+    )
+
+    return(chat_response.choices[0].message.content)
+
+    
+image_path = r"C:\Users\HP ZBOOK\Downloads\a35902a2ce0039f94b6fc5886b2db0cf.jpg"
+print(describe_image(image_path))
+    
+    
